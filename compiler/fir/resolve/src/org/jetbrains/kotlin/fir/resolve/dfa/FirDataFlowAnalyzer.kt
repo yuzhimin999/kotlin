b6e3d25a33def00f23f8a62a3ec24330cfb4eba6
Dmitriy Novozhilov <dmitriy.novozhilov@jetbrains.com>
2020-03-27 14:48:08 +0300

    [FIR] Add control flow graph for class initialization

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/dfa/FirDataFlowAnalyzer.kt
@@ -160,10 +160,30 @@ abstract class FirDataFlowAnalyzer<FLOW : Flow>(
         enterNode.flow = enterNode.flow.fork()
     }
 
-    // ----------------------------------- Anonymous object -----------------------------------
+    // ----------------------------------- Classes -----------------------------------
 
-    fun exitAnonymousObject(anonymousObject: FirAnonymousObject) {
-        graphBuilder.exitAnonymousObject(anonymousObject).mergeIncomingFlow()
+    fun enterClass() {
+        graphBuilder.enterClass()
+    }
+
+    fun exitClass() {
+        graphBuilder.exitClass()
+    }
+
+    fun exitRegularClass(klass: FirRegularClass): ControlFlowGraph {
+        return graphBuilder.exitClass(klass)
+    }
+
+    fun exitLocalClass(klass: FirRegularClass): ControlFlowGraph {
+        val (node, controlFlowGraph) = graphBuilder.exitLocalClass(klass)
+        node.mergeIncomingFlow()
+        return controlFlowGraph
+    }
+
+    fun exitAnonymousObject(anonymousObject: FirAnonymousObject): ControlFlowGraph {
+        val (node, controlFlowGraph) = graphBuilder.exitAnonymousObject(anonymousObject)
+        node.mergeIncomingFlow()
+        return controlFlowGraph
     }
 
     // ----------------------------------- Property -----------------------------------
